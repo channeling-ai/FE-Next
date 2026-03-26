56078198a5acbb05d53ed2904b571ee1a13462d4
MunJinYeong <moonjinyung64@gmail.com>
2026-03-26 14:15:18 +0900

    feat: textfield 컴포넌트 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
+'use client'
+import { useState } from 'react'
+
 export default function LandingPage() {
+  const [val, setVal] = useState('')
+
   return (
-    <main>
+    <main className="min-h-screen bg-bg-0 flex flex-col items-center justify-center p-8 gap-8">
+
     </main>
   )
 }
