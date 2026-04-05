c76c3b2746432b9259e2ce2f8a5214d172c47833
Sejeong Kim <sejeonggim68@gmail.com>
2026-04-05 19:21:30 +0900

    feat: 푸터 구현

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,23 +4,29 @@ import Link from 'next/link';
 
 export const Footer = () => {
     return (
-        <div className="fixed bottom-0 w-full desktop:left-18 desktop:w-[calc(100%-72px)] desktop:border-8 desktop:border-t-0 desktop:border-surface desktop:rounded-lg">
-            <footer
-                className="flex justify-center items-center py-8 desktop:rounded-lg font-footer-fixed"
-                style={{
-                    background: 'linear-gradient(180deg, rgba(45, 7, 9, 0.20) 0%, var(--primary-50, #2D0709) 50%)',
-                }}
-            >
-                <div className="grid grid-cols-2 tablet:grid-cols-4 gap-y-4 gap-x-[56px] tablet:gap-x-[69px] text-center">
-                    <Link href="/terms">회사 소개</Link>
-                    <Link href="/privacy">서비스 이용약관</Link>
-
-                    <a href="https://open.kakao.com/o/sTPlNEvh" target="_blank" rel="noopener noreferrer">
-                        개인정보처리방침
-                    </a>
-                    <a href="https://open.kakao.com/o/sTPlNEvh" target="_blank" rel="noopener noreferrer">
+        <div className="fixed bottom-0 w-full desktop:left-18 desktop:w-[calc(100%-72px)]">
+            <footer className="flex flex-col justify-center items-center py-8 desktop:rounded-lg font-caption-12r text-text-tertiary">
+                <div className="flex flex-wrap text-center">
+                    <Link href="/docs/terms" className="px-4 border-r border-border-subtle last:border-none">
+                        회사 소개
+                    </Link>
+                    <Link href="/docs/terms" className="px-4 border-r border-border-subtle last:border-none">
+                        서비스 이용약관
+                    </Link>
+                    <Link href="/docs/privacy" className="px-4 border-r border-border-subtle last:border-none">
+                        개인정보 처리방침
+                    </Link>
+                    <a
+                        href="https://open.kakao.com/o/sTPlNEvh"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="px-4 border-r border-border-subtle last:border-none"
+                    >
                         문의하기
                     </a>
+                </div>
+                <div className="mt-6 font-caption-12r text-text-tertiary text-center">
+                    © 2025 Chaneling. All rights reserved.
                 </div>
             </footer>
         </div>
