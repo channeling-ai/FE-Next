836445aa235b2d2ba049801951c4f4015a0f98e5
MunJinYeong <moonjinyung64@gmail.com>
2026-07-08 02:43:51 +0900

    refactor: 아이디어 타이포 스케일 정리

--- src/app/(protected)/ideas/_components/saved-idea-card.tsx
+++ src/app/(protected)/ideas/_components/saved-idea-card.tsx
@@ -25,12 +25,12 @@ export default function SavedIdeaCard({ onClick }: SaveCardProps) {
                 </button>
             </div>
             <h3 className="flex-1 line-clamp-1 font-body-16sb text-text-primary">코케트(coquette) 패션 인사</h3>
-            <p className="min-h-[calc(1em*1.5*2)] line-clamp-2 font-caption-14r text-text-secondary">
+            <p className="min-h-[calc(1em*1.5*2)] line-clamp-2 font-body-14r text-text-secondary">
                 가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가가
             </p>
             <div className="w-82 h-px bg-border-default mt-4" />
             <div className="flex flex-row flex-wrap gap-2">
-                <p className="px-2 py-1 rounded-[10px] bg-text-brand/8 text-text-brand font-body-16m">#ddd</p>
+                <p className="px-2 py-1 rounded-[10px] bg-text-brand/8 text-text-brand font-caption-12m">#ddd</p>
             </div>
         </div>
     )
