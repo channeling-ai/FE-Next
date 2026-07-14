04732848e31d7495b5560644ca4556ef31a84380
yoonvinjeong <anaabc1004@naver.com>
2026-07-15 01:26:49 +0900

    feat: 페이지 레이아웃 수정

--- src/app/(protected)/reports/_components/VideoCard.tsx
+++ src/app/(protected)/reports/_components/VideoCard.tsx
@@ -1,11 +1,20 @@
 interface VideoCardProps {
     title?: string
     leftside?: string
+    leftsideamount?: string
     rightside?: string
+    rightsideamount?: string
     period?: string
 }
 
-export default function VideoCard({ title, leftside, rightside, period }: VideoCardProps) {
+export default function VideoCard({
+    title,
+    leftside,
+    leftsideamount,
+    rightside,
+    rightsideamount,
+    period,
+}: VideoCardProps) {
     return (
         <div className="flex flex-col max-w-82 border border-transparent hover:border-border-active rounded-[20px]">
             <div className="flex w-full h-46 bg-transparent rounded-t-[20px]"></div>
@@ -14,8 +23,10 @@ export default function VideoCard({ title, leftside, rightside, period }: VideoC
                 <div className="self-stretch text-text-primary font-body-16sb line-clamp-2">{title}</div>
                 <div className="flex items-start gap-[8.816px] self-stretch">
                     <div className="text-text-secondary font-body-14r">{leftside}</div>
+                    {leftsideamount && <div className="text-text-secondary font-body-14r pl-1">{leftsideamount}</div>}
                     <div className="text-gray-600 font-body-14r">·</div>
                     <div className="text-text-secondary font-body-14r">{rightside}</div>
+                    {rightsideamount && <div className="text-text-secondary font-body-14r pl-1">{rightsideamount}</div>}
                 </div>
             </div>
         </div>
